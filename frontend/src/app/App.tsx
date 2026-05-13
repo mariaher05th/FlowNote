5e83f807dce1015c948864ecf9a6b7beb2ea96bc
vpaulina14 <paulinavalenciac123@gmail.com>
2026-05-13 12:27:27 -0500

    vista login y registro

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import { Dashboard } from './components/Dashboard';
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 export default function App() {
+  const [screen, setScreen] = useState("login");
   return (
     <div style={{ backgroundColor: '#F6F4FB', minHeight: '100vh', color: '#2F2840' }}>
-      <Dashboard />
+      
+      {screen === "login" && (
+        <Login
+          onLogin={() => setScreen("dashboard")}
+          goToRegister={() => setScreen("register")}
+        />
+      )}
+
+      {screen === "register" && (
+        <Register
+          onRegister={() => setScreen("dashboard")}
+          goToLogin={() => setScreen("login")}
+        />
+      )}
+
+      {screen === "dashboard" && <Dashboard />}
+
+
+
     </div>
   );
 }
+
+
