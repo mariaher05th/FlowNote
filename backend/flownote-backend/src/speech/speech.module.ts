70d10ab198a0730c71804cd09a1386380f1e7428
Daniel <69478471+mariskit@users.noreply.github.com>
2026-05-17 18:47:34 -0500

    Revert "Danielback"

--- backend/flownote-backend/src/speech/speech.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SpeechService } from './speech.service';
-import { SpeechController } from './speech.controller';
-
-@Module({
-  providers: [SpeechService],
-  controllers: [SpeechController],
-})
-export class SpeechModule {}
